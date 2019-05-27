b3f43f174f1f279ce4443d5adfe4ed0bf8e8fe37
MoTech <jemkarry@live.com>
2019-05-26 23:15:55 -0400

    final

--- wordcounter/views.py
+++ wordcounter/views.py
@@ -9,4 +9,11 @@ def count(request):
     fulltext = request.GET['fulltext']
     word_list = fulltext.split()
     count = len(word_list)
-    return render(request, 'count.html',{'fulltext':fulltext,'count':count})
+
+    word_dictionary = {}
+    for word in word_list:
+        if word in word_dictionary:
+            word_dictionary[word] +=1 #increase the ount
+        else:
+            word_dictionary[word] = 1 #add to dictionary
+    return render(request, 'count.html',{'fulltext':fulltext,'count':count,'word_dictionary':word_dictionary.items()})
